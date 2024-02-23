0fe9b6ad6c5b4d8e10bc74a502ae7e303cfcf4ab
charliehergenrother <charliehergenrother@yahoo.com>
2024-02-23 10:43:18 -0600

    February 23 brackets

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./scraper.py -b index.html -r excelPath/resume2024.csv -u menresumes.html
+./scraper.py -b index.html -r excelPath/resume2024.csv -u menresumes.html -o excelPath/presentbracketlist.csv
 cp index.html men.html
 ./scraper.py -b menfuture.html -f -o excelPath/bracketlist2024.csv -g menschedule.html
 ./scraper.py -w -b women.html -r excelPath/resume2024w.csv -u womenresumes.html

cdc4abeed5adcc36ab201aefdaa4fbe5f41c464c
Charlie Hergenrother <charliehergenrother@yahoo.com>
2024-11-11 09:03:15 -0600

    Nov 11 brackets

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,6 @@
 
 ./scraper.py -b index.html -r excelPath/resume2025.csv -u menresumes.html -o excelPath/presentbracketlist.csv
 cp index.html men.html
-./scraper.py -b menfuture.html -f -o excelPath/bracketlist2025.csv -g menschedule.html
-./scraper.py -w -b women.html -r excelPath/resume2025w.csv -u womenresumes.html
-./scraper.py -w -b womenfuture.html -f -o excelPath/bracketlist2025w.csv -g womenschedule.html
+./scraper.py -b menfuture.html -f -o excelPath/futurebracketlist2025.csv -g menschedule.html
+./scraper.py -w -b women.html -r excelPath/resume2025w.csv -u womenresumes.html -o excelPath/presentbracketlistw.csv
+./scraper.py -w -b womenfuture.html -f -o excelPath/futurebracketlist2025w.csv -g womenschedule.html
